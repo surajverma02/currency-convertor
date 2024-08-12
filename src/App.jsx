c8b33598ef48e5c058b74a37960ddd90fb87b868
surajverma02 <surajverma8730448@gmail.com>
2024-08-12 14:38:28 +0530

    file added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center mt-20">Currecy Convertor</h1>
+    </>
+  );
+}
+
+export default App;
